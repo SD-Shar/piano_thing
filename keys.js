62b1e2a588df7aa67d035f1c19d315a9c9663d71
Shar <reala039@osloskolen.no>
2026-05-20 13:48:26 +0200

    fra tastatur til reaksjon funekrgit status

--- keys.js
+++ keys.js
@@ -4,15 +4,30 @@
 
 const keys = document.querySelectorAll('.keys');
 
+
 keys.forEach((key) => {
   key.addEventListener('click', (e) => {
-    const clickedKey = event.target.dataset.list;
-      console.log(key);
+    const dataKey = key.getAttribute('data-key');
+
+    const clickedKey = e.target.dataset.key;
+    console.log(clickedKey);
+    
   })
 
-  
+  document.addEventListener('keydown', (event) => {
+  if (event.key == "a") {
+    console.log("a");
+    
+  } else if (event.key == "w"){
+    console.log("w");
+    
+  }
+})
+
 });
 
 
 
+
 // ref: http://geeksforgeeks.org/javascript/build-a-piano-using-html-css-and-javascript/
+// ref: https://stackoverflow.com/questions/74915532/how-do-you-detect-directional-arrow-keypresses-in-javascript
